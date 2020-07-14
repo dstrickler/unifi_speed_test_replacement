d34ee60491c88a5409a885f4575bdd54d3790737
Dave Strickler <dave@thestricklers.org>
2020-07-14 10:47:21 -0400

    Update update_unfi_controller_speed_test.sh
    Fixed parsing of JSON file hack. Needs a proper JSON parser. Added echos for logging.

--- update_unfi_controller_speed_test.sh
+++ update_unfi_controller_speed_test.sh
@@ -4,31 +4,39 @@
 # Warning: There is no error checking in here yet
 # Assumes exectable "bc" is installed by hand.
 #
-USERNAME="{put your username here}"
+USERNAME="{your username to log into the Unifi router}"
+UNIFI_ROUTER="{IP address of your Unifi router}"
 
 # Run the test and upload it to SQL
+echo "Running speedtest - stand by..."
 /usr/bin/speedtest --json > /tmp/speedtest.json
 
 # EPOC value
 EPOC=`date +%s`
 echo "$EPOC" > "lastrun"
-cat lastrun | ssh $USERNAME@10.0.1.1 "sudo tee /run/linkcheck/speedtest/lastrun"
+cat lastrun | ssh $USERNAME@$UNIFI_ROUTER "sudo tee /run/linkcheck/speedtest/lastrun"
 
 # ping
-LATENCY=`cut -d':' -f21 /tmp/speedtest.json | cut -d',' -f1 | awk '{$1=$1};1' | cut -d'}' -f1`
-echo $LATENCY
+echo "Sensing ping."
+LATENCY=`cut -d':' -f6 /tmp/speedtest.json | cut -d',' -f1 | awk '{$1=$1};1' | cut -d'}' -f1`
+echo "Ping set to $LATENCY"
 echo "$LATENCY" > "ping"
-cat ping | ssh $USERNAME@10.0.1.1 "sudo tee /run/linkcheck/speedtest/ping"
-
+cat ping | ssh $USERNAME@$UNIFI_ROUTER "sudo tee /run/linkcheck/speedtest/ping"
+echo "------------------------------"
 
 # Download speed
+echo "Sening download speed"
 DOWNLOAD=`cut -d':' -f2 /tmp/speedtest.json | cut -d',' -f1 | awk '{$1=$1};1'`
 DOWNLOAD=`echo "scale=2; $DOWNLOAD / 1000000" | bc`
+echo "Download set to $DOWNLOAD"
 echo "$DOWNLOAD" > "xput_down"
-cat xput_down | ssh $USERNAME@10.0.1.1 "sudo tee /run/linkcheck/speedtest/xput_down"
+cat xput_down | ssh $USERNAME@#UNIFI_ROUTER "sudo tee /run/linkcheck/speedtest/xput_down"
+echo "------------------------------"
 
 #upload speed
-UPLOAD=`cut -d':' -f3 /tmp/speedtest.json | cut -d',' -f1 | awk '{$1=$1};1'`
+echo "Sensing upload speed"
+UPLOAD=`cut -d':' -f7 /tmp/speedtest.json | cut -d',' -f1 | awk '{$1=$1};1'`
 UPLOAD=`echo "scale=2; $UPLOAD / 1000000" | bc`
+echo "Upload set to $UPLOAD"
 echo "$UPLOAD" > "xput_up"
-cat xput_up | ssh $USERNAME@10.0.1.1 "sudo tee /run/linkcheck/speedtest/xput_up"
+cat xput_up | ssh $USERNAME@$UNIFI_ROUTER "sudo tee /run/linkcheck/speedtest/xput_up"
